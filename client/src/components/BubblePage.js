fe513d1a01c119a634f0c436fb80d06909c14d05
Alison Law <alisparklaw@gmail.com>
2019-12-20 09:18:11 +0000

    login form, axioswitAuth

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
+import { axiosWithAuth } from "../auth/axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
-const BubblePage = () => {
+const BubblePage = (props) => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
-
+  useEffect(() => {
+  axiosWithAuth()
+  .get(`http://localhost:5000/api/colors`)
+  .then(res => {
+    setColorList(res.data);
+  })
+  .catch(error => {
+    // alert(error.response.data.message);
+    console.log(error.response.data.message);
+  });
+}, []);
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useRef }  from "react";
+import axios from "axios";
 
-const Login = () => {
+const Login = (props) => {
+  const usernameRef = useRef();
+  const passwordRef = useRef();
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const submit = () => {
+    axios
+      .post("http://localhost:5000/api/login", {
+        username: usernameRef.current.value,
+        password: passwordRef.current.value
+      })
+      .then(res => {
+        console.log(res);
+        // SUCCESS! Credentials are valid:
+        //   1- Put the token string in local storage under a 'token' key
+        localStorage.setItem("token", res.data.payload);
+        console.log(res.data.payload);
+        // localStorage.setItem('token', JSON.stringify(myArray));
+        //   2- Redirect users to the /quotes route
+        props.history.push("/BubblePage");
+      })
+      .catch(error => {
+        alert(error.message);
+        console.log(error.message);
+      });
+  };
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <h1>Welcome to the Bubble App!</h1><br/>
+      <div className="login">
+      <div className="login-inputs">
+        username <input ref={usernameRef} type="text" />
+        <br />
+        password <input ref={passwordRef} type="text" />
+      </div>
+
+      <div>
+        <button onClick={submit}>Submit</button>
+      </div>
+    </div>
     </>
   );
 };
